059d1469d5fd45d6b8919ff82000a4327ee345e5
Life_06 <jet4kryez1u@gmail.com>
2026-06-17 14:44:32 +0200

    module
    module the existing pages (home, project, about)

---
+++ config.py
@@ -0,0 +1,18 @@
+PAGE_TITLE = "Digital CV | Jeta Kryeziu"
+PAGE_ICON = "👋"
+
+NAME = "Camazing CATie"
+
+DESCRIPTION = """
+CATa SCATist specializing in Napping and Purring.
+With a knack for finding the purr-fect solution to any problem.
+"""
+
+EMAIL = "the_camazing_catie@gmail.com"
+
+LINKEDIN_URL = (
+    "https://www.linkedin.com/in/catie-cata-123456789/"
+)
+
+RESUME_FILE = "assets/cat_cv.pdf"
+PROFILE_PIC_FILE = "assets/profile-pic.jpeg"
---
+++ pages/projects.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+def show():
+    st.title("Projects")
+    st.write("\n")
+    st.write("🐁The Great Mouse Patrol")
+    st.write(
+        "Monitored the house for suspicious mouse activity and completed nightly security checks.")
+
+    st.write("\n")
+    st.write("📍Sunbeam Location Tracker")
+    st.write(
+        "Identified the warmest sunny spots in the house and created an optimized daily nap schedule.")
+
+    st.write("\n")
+    st.write("📦Cardboard Box Quality Testing")
+    st.write(
+        "Tested boxes of different sizes for comfort, durability, and overall sitting potential.")
